eb55778c3dfb34580e7253749f648d45247f9700
higio97 <92028330+higio97@users.noreply.github.com>
2022-12-19 18:21:06 +0700

    Update send_handler.py

--- plugins/command/send_handler.py
+++ plugins/command/send_handler.py
@@ -11,13 +11,13 @@ async def send_with_pic_handler(client: Client, msg: types.Message, key: str, ha
     if msg.text or msg.photo or msg.video or msg.voice:
         menfess = user.menfess
         all_menfess = user.all_menfess
-        coin = user.coin
+        dm = user.dm
         if menfess >= config.batas_kirim:
             if user.status == 'member' or user.status == 'talent':
-                if coin >= config.biaya_kirim:
-                    coin = user.coin - config.biaya_kirim
+                if dm >= config.biaya_kirim:
+                    dm = user.dm - config.biaya_kirim
                 else:
-                    return await msg.reply(f'🙅🏻‍♀️ post gagal terkirim. kamu hari ini telah mengirim ke menfess sebanyak {menfess}/{config.batas_kirim} kali.serta coin mu kurang untuk mengirim menfess diluar batas harian., kamu dapat mengirim menfess kembali pada hari esok.\n\n waktu reset jam 1 pagi', quote=True)
+                    return await msg.reply(f'🙅🏻‍♀️ Post Gagal Terkirim. Kamu Hari Ini Telah Mengirim Pesan Sebanyak {menfess}/{config.batas_kirim} Kali. Serta Diamond Mu Kurang Untuk Mengirim Pesan Diluar Batas Harian., Kamu Dapat Mengirim Pesan Kembali Pada Hari Besok.\n\n Waktu Reset Jam 1 Pagi', quote=True)
 
         if key == hastag[0]:
             picture = config.pic_girl
@@ -30,10 +30,10 @@ async def send_with_pic_handler(client: Client, msg: types.Message, key: str, ha
 
         kirim = await client.send_photo(config.channel_1, picture, caption, caption_entities=entities)
         await helper.send_to_channel_log(type="log_channel", link=link + str(kirim.id))
-        await db.update_menfess(coin, menfess, all_menfess)
-        await msg.reply(f"pesan telah berhasil terkirim. hari ini kamu telah mengirim menfess sebanyak {menfess + 1}/{config.batas_kirim} . kamu dapat mengirim menfess sebanyak {config.batas_kirim} kali dalam sehari\n\nwaktu reset setiap jam 1 pagi\n<a href='{link + str(kirim.id)}'>check pesan kamu</a>")
+        await db.update_menfess(cdm, menfess, all_menfess)
+        await msg.reply(f"Pesan Telah Berhasil Terkirim. Hari Ini Kamu Telah Mengirim Pesan Sebanyak {menfess + 1}/{config.batas_kirim} . Kamu Dapat Mengirim Pesan Sebanyak {config.batas_kirim} Kali Dalam Sehari\n\nWaktu Reset Setiap Jam 1 Pagi\n<a href='{link + str(kirim.id)}'>Check Pesan Kamu</a>")
     else:
-        await msg.reply('media yang didukung photo, video dan voice')
+        await msg.reply('Media Yang Didukung Photo, Video Dan Voice')
 
 async def send_menfess_handler(client: Client, msg: types.Message):
     helper = Helper(client, msg)
@@ -53,19 +53,19 @@ async def send_menfess_handler(client: Client, msg: types.Message):
 
         menfess = db_user.menfess
         all_menfess = db_user.all_menfess
-        coin = db_user.coin
+        dm = db_user.dm
         if menfess >= config.batas_kirim:
             if db_user.status == 'member' or db_user.status == 'talent':
-                if coin >= config.biaya_kirim:
-                    coin = db_user.coin - config.biaya_kirim
+                if dm >= config.biaya_kirim:
+                    dm = db_user.dm - config.biaya_kirim
                 else:
-                    return await msg.reply(f'🙅🏻‍♀️ post gagal terkirim. kamu hari ini telah mengirim ke menfess sebanyak {menfess}/{config.batas_kirim} kali.serta coin mu kurang untuk mengirim menfess diluar batas harian., kamu dapat mengirim menfess kembali pada hari esok.\n\n waktu reset jam 1 pagi', quote=True)
+                    return await msg.reply(f'🙅🏻‍♀️ Post Gagal Terkirim. Kamu Hari Ini Telah Mengirim Ke Pesan Sebanyak {menfess}/{config.batas_kirim} Kali. Serta Diamond Mu Kurang Untuk Mengirim Pesan Diluar Batas Harian., Kamu Dapat Mengirim Pesan Kembali Pada Hari Besok.\n\nWaktu Reset Jam 1 Pagi', quote=True)
 
         link = await get_link()
         kirim = await client.copy_message(config.channel_1, msg.from_user.id, msg.id)
         await helper.send_to_channel_log(type="log_channel", link=link + str(kirim.id))
-        await db.update_menfess(coin, menfess, all_menfess)
-        await msg.reply(f"pesan telah berhasil terkirim. hari ini kamu telah mengirim menfess sebanyak {menfess + 1}/{config.batas_kirim} . kamu dapat mengirim menfess sebanyak {config.batas_kirim} kali dalam sehari\n\nwaktu reset setiap jam 1 pagi\n<a href='{link + str(kirim.id)}'>check pesan kamu</a>")
+        await db.update_menfess(dm, menfess, all_menfess)
+        await msg.reply(f"Pesan Telah Berhasil Terkirim. Hari Ini Kamu Telah Mengirim Pesan Sebanyak {menfess + 1}/{config.batas_kirim} . Kamu Dapat Mengirim Pesan Sebanyak {config.batas_kirim} Kali Dalam Sehari\n\nWaktu Reset Setiap Jam 1 Pagi\n<a href='{link + str(kirim.id)}'>Check Pesan Kamu</a>")
     else:
         await msg.reply('media yang didukung photo, video dan voice')
 
@@ -73,50 +73,50 @@ async def get_link():
     anu = str(config.channel_1).split('-100')[1]
     return f"https://t.me/c/{anu}/"
 
-async def transfer_coin_handler(client: Client, msg: types.Message):
-    if re.search(r"^[\/]tf_coin(\s|\n)*$", msg.text or msg.caption):
-        err = "<i>perintah salah /tf_coin [jmlh_coin]</i>" if msg.reply_to_message else "<i>perintah salah /tf_coin [id_user] [jmlh_coin]</i>"
+async def transfer_dm_handler(client: Client, msg: types.Message):
+    if re.search(r"^[\/]tf_dm(\s|\n)*$", msg.text or msg.caption):
+        err = "<i>Perintah Salah /tf_dm [jmlh_dm]</i>" if msg.reply_to_message else "<i>Perintah Salah /tf_dm [id_user] [jmlh_dm]</i>"
         return await msg.reply(err, True)
     helper = Helper(client, msg)
-    if re.search(r"^[\/]tf_coin\s(\d+)(\s(\d+))?", msg.text or msg.caption):
-        x = re.search(r"^[\/]tf_coin\s(\d+)(\s(\d+))$", msg.text or msg.caption)
+    if re.search(r"^[\/]tf_dm\s(\d+)(\s(\d+))?", msg.text or msg.caption):
+        x = re.search(r"^[\/]tf_dm\s(\d+)(\s(\d+))$", msg.text or msg.caption)
         if x:
             target = x.group(1)
-            coin = x.group(3)
-        y = re.search(r"^[\/]tf_coin\s(\d+)$", msg.text or msg.caption)
+            dm = x.group(3)
+        y = re.search(r"^[\/]tf_dm\s(\d+)$", msg.text or msg.caption)
         if y:
             if msg.reply_to_message:
                 if msg.reply_to_message.from_user.is_bot == True:
-                    return await msg.reply('🤖Bot tidak dapat ditranfer coin', True)
+                    return await msg.reply('🤖Bot Tidak Dapat Ditranfer dm', True)
                 elif msg.reply_to_message.sender_chat:
-                    return await msg.reply('channel tidak dapat ditranfer coin', True)
+                    return await msg.reply('Channel Tidak Dapat Ditranfer dm', True)
                 else:
                     target = msg.reply_to_message.from_user.id
-                    coin = y.group(1)
+                    dm = y.group(1)
             else:
                 return await msg.reply('sambil mereply sebuah pesan', True)
         
         if msg.from_user.id == int(target):
-            return await msg.reply('<i>Tidak dapat transfer coin untuk diri sendiri</i>', True)
+            return await msg.reply('<i>Tidak Dapat Transfer Diamond Untuk Diri Sendiri</i>', True)
 
         user_db = Database(msg.from_user.id)
         anu = user_db.get_data_pelanggan()
-        my_coin = anu.coin
-        if my_coin >= int(coin):
+        my_dm = anu.dm
+        if my_dm >= int(dm):
             db_target = Database(int(target))
             if await db_target.cek_user_didatabase():
                 target_db = db_target.get_data_pelanggan()
-                ditransfer = my_coin - int(coin)
-                diterima = target_db.coin + int(coin)
+                ditransfer = my_dm - int(dm)
+                diterima = target_db.dm + int(dm)
                 nama = "Admin" if anu.status == 'owner' or anu.status == 'admin' else msg.from_user.first_name
                 nama = await helper.escapeHTML(nama)
                 try:
-                    await client.send_message(target, f"Coin berhasil ditambahkan senilai {coin} coin, cek /status\n└Oleh <a href='tg://user?id={msg.from_user.id}'>{nama}</a>")
-                    await user_db.transfer_coin(ditransfer, diterima, target_db.coin_full, int(target))
-                    await msg.reply(f'<i>berhasil transfer coin sebesar {coin} coin💰</i>', True)
+                    await client.send_message(target, f"Diamond Berhasil Ditambahkan Senilai {dm} Diamond, Cek /status\n└Oleh <a href='tg://user?id={msg.from_user.id}'>{nama}</a>")
+                    await user_db.transfer_dm(ditransfer, diterima, target_db.dm_full, int(target))
+                    await msg.reply(f'<i>Berhasil Transfer Diamond Sebesar {dm} Diamond 💎</i>', True)
                 except Exception as e:
                     return await msg.reply_text(
-                        text=f"❌<i>Terjadi kesalahan, sepertinya user memblokir bot</i>\n\n{e}", quote=True,
+                        text=f"❌<i>Terjadi Kesalahan, Sepertinya User Memblokir Bot</i>\n\n{e}", quote=True,
                         parse_mode=enums.ParseMode.HTML
                     )
             else:
@@ -125,4 +125,4 @@ async def transfer_coin_handler(client: Client, msg: types.Message):
                     parse_mode=enums.ParseMode.HTML
                 )
         else:
-            return await msg.reply(f'<i>coin kamu ({my_coin}) tidak dapat transfer coin.</i>', True)
+            return await msg.reply(f'<i>Diamond Kamu ({my_dm}) Tidak Dapat Transfer Diamond.</i>', True)
